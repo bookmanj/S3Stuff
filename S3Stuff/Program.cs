d429c3ee57425357d48baa878880423e12ffca6f
Jay Perusse <jay.perusse@outsystems.com>
2020-12-17 10:59:05 -0600

    Add project files.

---
+++ S3Stuff/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UploadToS3Demo
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      // preparing our file and directory names
+      string fileToBackup = @"C:\S3Temp\pluralsights3bucketjay\LICENSE"; // test file
+      string myBucketName = "pluralsights3bucketjay"; //your s3 bucket name goes here
+      string s3DirectoryName = "justdemodirectory";
+      string s3FileName = @"LICENSE";
+
+      AmazonUploader myUploader = new AmazonUploader();
+      await myUploader.sendMyFileToS3(fileToBackup, myBucketName, s3DirectoryName, s3FileName);
+    }
+  }
+}
